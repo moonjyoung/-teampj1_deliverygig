850107461b9ea130fcb4d4ae9dcbccf1b3a20eca
asakoLee <duddms613@naver.com>
2023-01-19 18:16:44 +0900

    third commit

--- src/main/java/com/deliverygig/moonjyoung/api/CustomerInfoController.java
+++ src/main/java/com/deliverygig/moonjyoung/api/CustomerInfoController.java
@@ -7,10 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +28,7 @@ public class CustomerInfoController {
     @Autowired
     CustomerRepository cRepo;
     @Autowired CustomerInfoService cService;
-    @PutMapping("/join")
+    @PostMapping("/join")
     public ResponseEntity<Object> memberJoin(@RequestBody CustomerAddVO data) {
         Map<String, Object> resultMap = cService.addMember(data);
         return new ResponseEntity<Object>(resultMap, (HttpStatus) resultMap.get("code"));
@@ -62,7 +60,7 @@ public class CustomerInfoController {
         Map<String, Object> resultMap = cService.AvailableMember(data, type);
         return new ResponseEntity<Object>(resultMap, (HttpStatus) resultMap.get("code"));
     }
-    @PatchMapping("/update/{type}")
+    @PostMapping("/update/{type}")
     public ResponseEntity<Object> memberUpdate(@RequestBody UpdateCustomerInfoVO data2, @PathVariable String type, HttpSession session) throws Exception{
         Map<String, Object> resultMap = cService.UpdateMember(data2, type, session);
         return new ResponseEntity<Object>(resultMap, (HttpStatus) resultMap.get("code"));
