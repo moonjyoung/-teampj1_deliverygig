bbb8f6b7bbe7e18bef7cb3e64f24b86636f3bcf6
dev <moonjyoung1214@gmail.com>
2023-01-30 14:44:44 +0900

    moonjyoung 230130 ver.02

--- src/main/java/com/deliverygig/moonjyoung/vo/food/ShowMenuInfoVO.java
+++ src/main/java/com/deliverygig/moonjyoung/vo/food/ShowMenuInfoVO.java
@@ -6,13 +6,13 @@ import lombok.Data;
 
 @Data
 public class ShowMenuInfoVO {
-    private Long seq;
+    private Long menuSeq;
     private String name;
     private Integer price;
     private Integer discountPrice;
 
     public ShowMenuInfoVO(FoodMenuInfoEntity entity) {
-        this.seq = entity.getFmiSeq();
+        this.menuSeq = entity.getFmiSeq();
         this.name = entity.getFmiName();
         this.price = entity.getFmiPrice();
         this.discountPrice = (int)(entity.getFmiPrice()*(1-entity.getFoodCategoryEntity().getStoreInfoEntity().getSiDiscount()/100.0));
